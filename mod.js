9e71ec059fdd457d2fa33d89acbcfe23185d9a80
PascalSalesch <pascal.b.salesch@gmail.com>
2020-12-06 13:35:30 +0100

    linted

--- mod.js
+++ mod.js
@@ -1,10 +1,14 @@
 // Copyright 2020 Pascal Salesch <Pascal.B.Salesch@gmail.com>. All rights reserved. MIT license.
 
-const url = new URL(import.meta.url)
-const hash = url.hash.substr(1)
-const params = url.searchParams
-const valueName = hash || params.get('eval')
-const async = url.searchParams.get('async') === 'true'
-const value = async ? (await (new (Object.getPrototypeOf(async () => {}).constructor)(`return ${valueName}`)())) : (new Function(`return ${valueName}`))()
+const url = new URL(import.meta.url);
+const hash = url.hash.substr(1);
+const params = url.searchParams;
+const expression = `return ${hash || params.get("eval")}`;
+const async = params.get("async") === "true";
+const value = async
+  ? (await (new (Object.getPrototypeOf(async () => {}).constructor)(
+    expression,
+  )()))
+  : (new Function(expression))();
 
-export default value
+export default value;
